2a93982bd2ff9c2b2298fcb6e8b21be18d52f2fe
Nowu <nowu.user@gmail.com>
2025-08-06 20:16:26 -0500

    [day05] refactor and part 2 done

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -1,4 +1,3 @@
-#![expect(unused_variables)]
 use anyhow::Result;
 
 fn get_rules(rules: &[(usize, usize)], e: &usize) -> (Vec<usize>, Vec<usize>) {
@@ -11,40 +10,78 @@ fn get_rules(rules: &[(usize, usize)], e: &usize) -> (Vec<usize>, Vec<usize>) {
             pre.push(*p);
         }
     }
+
     (pre, post)
 }
 
-pub fn part_1(input: &str) -> Result<usize> {
+fn check_order(rules: &[(usize, usize)], update: &[usize]) -> bool {
+    update.iter().enumerate().all(|(i, e)| {
+        let (pre_rules, post_rules) = get_rules(rules, e);
+        !(update[i + 1..]).iter().any(|x| pre_rules.contains(x))
+            && !(update[..i]).iter().any(|x| post_rules.contains(x))
+    })
+}
+
+fn parse_input(input: &str) -> (Vec<(usize, usize)>, Vec<Vec<usize>>) {
     let (rules, updates) = input.split_once("\n\n").unwrap();
-    let rules: Vec<_> = rules
+    let rules = rules
         .lines()
         .map(|l| {
             let (f, s) = l.split_once("|").unwrap();
-            (f.parse::<usize>().unwrap(), s.parse::<usize>().unwrap())
+            (f.parse().unwrap(), s.parse().unwrap())
         })
         .collect();
     let updates = updates
         .lines()
-        .map(|l| {
-            l.split(",")
-                .map(|w| w.parse::<usize>().unwrap())
-                .collect::<Vec<_>>()
-        })
-        .filter(|values| {
-            values.iter().enumerate().all(|(i, e)| {
-                let (pre_rules, post_rules) = get_rules(&rules, e);
-                !(&values[i + 1..]).iter().any(|x| pre_rules.contains(x))
-                    && !(&values[..i]).iter().any(|x| post_rules.contains(x))
-            })
-        })
+        .map(|l| l.split(",").map(|w| w.parse().unwrap()).collect())
+        .collect();
+    (rules, updates)
+}
+
+pub fn part_1(input: &str) -> Result<usize> {
+    let (rules, updates) = parse_input(input);
+
+    let updates = updates
+        .into_iter()
+        .filter(|update| check_order(&rules, update))
         .map(|values| values[values.len() / 2])
         .sum();
 
     Ok(updates)
 }
 
+// TODO optimizar this
 pub fn part_2(input: &str) -> Result<usize> {
-    Ok(0)
+    let (rules, updates) = parse_input(input);
+
+    let updates = updates
+        .into_iter()
+        .filter(|updates| !check_order(&rules, updates))
+        .map(|mut update| {
+            loop {
+                let i = match update.iter().enumerate().position(|(k, v)| {
+                    let (pre_rules, _) = get_rules(&rules, v);
+                    update[k + 1..].iter().any(|x| pre_rules.contains(x))
+                }) {
+                    Some(i) => i,
+                    None => break,
+                };
+
+                let (pre_rules, _) = get_rules(&rules, &update[i]);
+                let target = update[i + 1..]
+                    .iter()
+                    .find(|x| pre_rules.contains(x))
+                    .unwrap();
+                let pos1 = update.iter().position(|&x| x == update[i]).unwrap();
+                let pos2 = update.iter().position(|&x| x == *target).unwrap();
+                update.swap(pos1, pos2);
+            }
+            update
+        })
+        .map(|values| values[values.len() / 2])
+        .sum();
+
+    Ok(updates)
 }
 
 #[cfg(test)]
@@ -85,7 +122,7 @@ mod tests {
 97,13,75,29,47"
         };
         assert_eq!(part_1(input)?, 143);
-        assert_eq!(part_2(input)?, 0);
+        assert_eq!(part_2(input)?, 123);
         Ok(())
     }
 }
