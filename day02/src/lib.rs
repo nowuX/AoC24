47dbbd6935aaf09aa0c4ab82d8ec61ba417652a7
Nowu <nowu.user@gmail.com>
2025-08-08 12:15:46 -0500

    [day07] i dont feel good ahh moments 😭

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -11,18 +11,18 @@ fn hasnt_bad_level(line: &[i64]) -> bool {
         (1..=3).contains(&diff)
     })
 }
-fn is_safe(line: &[i64]) -> bool {
-    is_sorted(line) && hasnt_bad_level(line)
-}
 
 fn is_safe_without_n(line: &[i64]) -> bool {
-    if is_safe(line) {
+    if is_sorted(line) && hasnt_bad_level(line) {
         return true;
     }
 
     (0..line.len()).any(|i| {
         let without_i = [&line[0..i], &line[i + 1..]].concat();
-        is_safe(&without_i)
+        {
+            let line: &[i64] = &without_i;
+            is_sorted(line) && hasnt_bad_level(line)
+        }
     })
 }
 
@@ -30,11 +30,7 @@ pub fn part_1(input: &str) -> Result<usize> {
     let reports = input
         .lines()
         .map(|line| parse_split(line, " "))
-        .filter(|line| {
-            let sorted = is_sorted(line);
-            let no_bad_level = hasnt_bad_level(line);
-            sorted & no_bad_level
-        })
+        .filter(|line| is_sorted(line) && hasnt_bad_level(line))
         .count();
     Ok(reports)
 }
@@ -95,7 +91,7 @@ where
 
 #[cfg(test)]
 mod tests {
-    use crate::{hasnt_bad_level, is_safe_without_n, is_sorted, part_1, part_2};
+    use crate::{part_1, part_2};
     use anyhow::Result;
 
     #[test]
@@ -104,54 +100,5 @@ mod tests {
         assert_eq!(part_1(input)?, 2);
         assert_eq!(part_2(input)?, 4);
         Ok(())
-    }
-
-    #[test]
-    fn sort() {
-        // reverse and normal
-        assert!(is_sorted(&[1, 2, 3, 4]));
-        assert!(is_sorted(&[4, 3, 2, 1]));
-        // double value
-        assert!(!is_sorted(&[1, 3, 3, 4]));
-        assert!(!is_sorted(&[4, 3, 3, 1]));
-    }
-
-    #[test]
-    fn bad_level() {
-        // normal 1 diff
-        assert!(hasnt_bad_level(&[1, 2, 3, 4, 5]));
-        assert!(hasnt_bad_level(&[5, 4, 3, 2, 1]));
-        // normal 2 diff
-        assert!(hasnt_bad_level(&[1, 3, 5, 7, 9]));
-        assert!(hasnt_bad_level(&[9, 7, 5, 3, 1]));
-        // normal 3 diff
-        assert!(hasnt_bad_level(&[1, 4, 7, 10, 13]));
-        assert!(hasnt_bad_level(&[13, 10, 7, 4, 1]));
-        // reject 0 diff (dups)
-        assert!(!hasnt_bad_level(&[1, 1, 2, 2, 3]));
-        assert!(!hasnt_bad_level(&[3, 2, 2, 1, 1]));
-        // reject 4 diff
-        assert!(!hasnt_bad_level(&[0, 4, 8, 12, 16]));
-        // reject +3 diff
-        assert!(!hasnt_bad_level(&[0, 5, 11, 18, 27]));
-    }
-
-    #[test]
-    fn tolerate_bad_level() {
-        let mat = [
-            // element in any place in
-            vec![1, 3, 2, 3, 4, 5],
-            vec![5, 6, 4, 3, 2, 1],
-            // elem in sides
-            vec![2, 1, 2, 3, 4, 5],
-            vec![4, 5, 4, 3, 2, 1],
-            // diff check
-            vec![1, 2, 3, 4, 5],
-        ];
-        for line in mat {
-            assert!(is_safe_without_n(&line))
-        }
-
-        // el error era que verifica conficion por condicion aunque igual medio sus
     }
 }
--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -1,40 +1,102 @@
-#![expect(unused_variables)]
 use anyhow::Result;
 use utils::parse_split;
 
-fn op_combinations(n: usize) -> impl Iterator<Item = Vec<bool>> {
-    let total = 1 << n;
-    (0..total).map(move |i| (0..n).map(move |j| (i >> (n - 1 - j)) & 1 == 1).collect())
+#[derive(Debug)]
+enum Op {
+    Add,
+    Mul,
+    Comb,
 }
 
-fn eval_expr(numbers: &[usize], operators: &[bool], target: &usize) -> bool {
-    let mut result = numbers[0];
-    for (i, op) in operators.iter().enumerate() {
-        // lil opt
-        if result > *target {
-            return false;
-        }
-        match op {
-            true => result += numbers[i + 1],
-            false => result *= numbers[i + 1],
+fn op_combinations(n: usize, base: usize) -> impl Iterator<Item = Vec<Op>> {
+    let total = base.pow(n as u32);
+    (0..total).map(move |i| {
+        (0..n)
+            .map(move |j| {
+                let digit = if base == 2 {
+                    (i >> (n - 1 - j)) & 1
+                } else {
+                    (i / base.pow((n - 1 - j) as u32)) % base
+                };
+                match digit {
+                    0 => Op::Add,
+                    1 => Op::Mul,
+                    2 => Op::Comb,
+                    _ => unreachable!(),
+                }
+            })
+            .collect()
+    })
+}
+
+// target op wait
+fn eval_rec(ns: &[usize], ops: &[Op]) -> usize {
+    if ns.len() == 1 {
+        return ns[0];
+    }
+    let (n, ns) = (ns[0], &ns[1..]);
+    let (op, ops) = (&ops[0], if ops.len() > 1 { &ops[1..] } else { &[] });
+    // println!("n={n:?},op={op:?}\tns={ns:?},ops={ops:?}");
+
+    match op {
+        Op::Add => n + eval_rec(ns, ops),
+        Op::Mul => n * eval_rec(ns, ops),
+        Op::Comb => {
+            let new_n = format!("{n}{}", ns[0]).parse::<usize>().unwrap();
+            // println!("new_n={new_n:?}");
+            if ns.len() == 1 {
+                // println!("\toperators[1..].is_empty()");
+                new_n
+            } else {
+                let ns = {
+                    let mut n = vec![new_n];
+                    n.extend_from_slice(&ns[1..]);
+                    n
+                };
+                eval_rec(&ns, ops)
+            }
         }
     }
+}
+
+fn eval_expr(numbers: &[usize], operators: &[Op], target: &usize) -> bool {
+    let result = eval_rec(&numbers, operators);
+    // println!("\tresult={result:?}");
     result == *target
 }
 
-fn eval_ecu(numbers: &[usize], target: &usize) -> Option<Vec<bool>> {
-    op_combinations(numbers.len() - 1).find(|ops| eval_expr(numbers, ops, target))
+fn valid_ecu(target: &usize, numbers: &[usize], base: usize) -> bool {
+    println!("\n==> target={target:?}, numbers={numbers:?}");
+    if op_combinations(numbers.len() - 1, base).any(|ops| eval_expr(numbers, &ops, target)) {
+        true
+    } else {
+        false
+    }
 }
 
 pub fn part_1(input: &str) -> Result<usize> {
-    let result = input
-        .lines()
-        .map(|l| {
-            l.split_once(": ")
-                .map(|(x, y)| (x.parse::<usize>().unwrap(), parse_split::<usize>(y, " ")))
-                .unwrap()
+    let ecu = input.lines().map(|l| {
+        l.split_once(": ")
+            .map(|(n, ns)| {
+                let n = n.parse::<usize>().unwrap();
+                let ns = parse_split::<usize>(ns, " ");
+                (n, ns)
+            })
+            .unwrap()
+    });
+    let result = ecu
+        .filter(|(target, numbers)| {
+            let numbers = {
+                let mut numbers = numbers.clone();
+                numbers.reverse();
+                numbers
+            };
+            let x = valid_ecu(target, &numbers, 2);
+            if x {
+                println!("✅✅, {target:?}");
+            }
+            x
         })
-        .filter(|(target, numbers)| eval_ecu(numbers, target).is_some())
         .map(|(target, _)| target)
         .sum();
 
@@ -42,7 +104,29 @@ pub fn part_1(input: &str) -> Result<usize> {
 }
 
 pub fn part_2(input: &str) -> Result<usize> {
-    Ok(0)
+    println!("=>");
+    let ecu = input.lines().map(|l| {
+        l.split_once(": ")
+            .map(|(n, ns)| {
+                let n = n.parse::<usize>().unwrap();
+                let ns = parse_split::<usize>(ns, " ");
+                (n, ns)
+            })
+            .unwrap()
+    });
+    let result = ecu
+        .filter(|(target, numbers)| {
+            let x = valid_ecu(target, &numbers, 3);
+            if x {
+                println!("✅✅, {target:?}");
+            }
+            x
+        })
+        .map(|(target, _)| target)
+        .sum();
+    println!("=>");
+
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -50,10 +134,7 @@ mod tests {
     use crate::{part_1, part_2};
     use anyhow::Result;
 
-    #[test]
-    fn test_day() -> Result<()> {
-        let input = {
-            "190: 10 19
+    const INPUT: &str = "190: 10 19
 3267: 81 40 27
 83: 17 5
 156: 15 6
@@ -61,10 +142,16 @@ mod tests {
 161011: 16 10 13
 192: 17 8 14
 21037: 9 7 18 13
-292: 11 6 16 20"
-        };
-        assert_eq!(part_1(input)?, 3749);
-        assert_eq!(part_2(input)?, 0);
+292: 11 6 16 20";
+    #[test]
+    fn test_day_part_1() -> Result<()> {
+        assert_eq!(part_1(INPUT)?, 3749);
+        Ok(())
+    }
+
+    #[test]
+    fn test_day_part_2() -> Result<()> {
+        assert_eq!(part_2(INPUT)?, 11387);
         Ok(())
     }
 }
